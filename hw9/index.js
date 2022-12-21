4437c336ff2e9d5757dd2e8bc2c682ae7b07d03e
alesiad <drakonova02@gmail.com>
2022-12-22 00:08:47 +0100

    remark code

--- hw9/index.js
+++ hw9/index.js
@@ -1,23 +1,25 @@
 function setValueLegs() {
-    let valuesLegs = prompt('Input values of legs (put a space between values)').split(' ');
+    let valuesLegs = prompt('Input values of legs (put a space between values)');
+    let valuesLegsArr = valuesLegs ? valuesLegs.split(' ') : [];
     
-    while(valuesLegs.length != 2) {
-        valuesLegs = prompt('Error: incorrect length of legs (default value is 2). Input new values').split(' ');
+    while(valuesLegsArr.length !== 2) {
+        valuesLegs = prompt('Error: incorrect length of legs (default value is 2). Input new values');
+        valuesLegsArr = valuesLegs ? valuesLegs.split(' ') : [];
     }
 
-    for(let i = 0; i < valuesLegs.length; ++i) {
-        while(isNaN(valuesLegs[i]) || valuesLegs[i] <= 0) {
-            valuesLegs[i] = prompt('Error: not valid value for leg (number > 0). Input new value for leg');
+    for(let i = 0; i < valuesLegsArr.length; ++i) {
+        while(isNaN(valuesLegsArr[i]) || valuesLegsArr[i] <= 0) {
+            valuesLegsArr[i] = prompt('Error: not valid value for leg (number > 0). Input new value for leg');
         }
     }
 
-    return valuesLegs;
+    return valuesLegsArr;
 }
 
 function sqrt(number) {
     let i = 1;
 
-    while (!(i * i > number)) {
+    while (i * i < number) {
         ++i;
     }
 
@@ -29,9 +31,9 @@ function hypotenuseWithMath(firstLeg, secondLeg) {
 }
 
 function hypotenuseWithoutMath(firstLeg, secondLeg) {
-    return sqrt((firstLeg * firstLeg + secondLeg * secondLeg).toFixed(0));
+    return sqrt(firstLeg ** 2 + secondLeg ** 2);
 }
 
 const valuesLegs = setValueLegs();
 
-alert(`Result with Math is ${hypotenuseWithMath(valuesLegs[0], valuesLegs[1])}\nResult without Math ~ ${hypotenuseWithoutMath(valuesLegs[0], valuesLegs[1])} (for natural number)`);
+alert(`Result with Math is ${hypotenuseWithMath(valuesLegs[0], valuesLegs[1])}\nResult without Math ~ ${hypotenuseWithoutMath(valuesLegs[0], valuesLegs[1])}`);
